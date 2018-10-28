e76ba512e46bdc96748c2956144199ed7dd9375b
ernestogo <ernesto_go@dlsu.edu.ph>
2018-10-28 15:39:24 +0800

    combined

---
+++ Movement+dialogue/Assets/Scripts/Conversation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour {
+
+    private int isConversing=0;
+
+    public int getIsConversing()
+    {
+        return isConversing;
+    }
+    public void setIsConversing(int i)
+    {
+        isConversing=i;
+    }
+    
+}
---
+++ Movement+dialogue/Assets/Scripts/Trigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+    public Conversation conv;
+    public Canvas canvas;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name.Equals("Conversation"))
+        {
+            conv.setIsConversing(1);
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (conv.getIsConversing() == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                canvas.gameObject.SetActive(false);
+                conv.setIsConversing(0);
+            }
+        }
+    }
+}
