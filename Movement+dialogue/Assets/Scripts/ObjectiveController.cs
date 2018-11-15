5846de0a4f3288ac6a56c81e5cfd5a0f7e42808b
KillerTrex1 <martmaster1@gmail.com>
2018-11-16 05:52:03 +0800

    Revert "With Audio"
    This reverts commit fd2f569ebbbe29e95b7c11f706457fdc5bf8a8da.

--- Movement+dialogue/Assets/Scripts/ObjectiveController.cs
+++ Movement+dialogue/Assets/Scripts/ObjectiveController.cs
@@ -7,27 +7,23 @@ public class ObjectiveController : MonoBehaviour {
     public Text UItext;
     public Text PastText;
     public Text CurrentText;
-    public AudioController Sound;
     // Use this for initialization
     public string CurrentO;
-    public int CChapter;
-    public int Ctask;
+    
     public void MissionComplete(int Chapter,int task)
     {
-        if (Chapter == 1 && task == 1&&CChapter==1&&Ctask==1)
+        if (Chapter == 1 && task == 1)
         {
-            //CurrentO= "Give Books to Girl (Press enter twice near her)";
-            //addPast(UItext.text,CurrentO);
-            //UItext.text = CurrentO;
-            //Ctask++;
-            UpdateChapter("Give Books to Girl (Press enter twice near her)");
-        }else if(Chapter ==1 && task == 2&& CChapter == 1 && Ctask == 2)
+            CurrentO= "Give Books to Girl (Press enter twice near her)";
+            addPast(UItext.text,CurrentO);
+            UItext.text = CurrentO;
+            task++;
+        }else if(Chapter ==1 && task == 2)
         {
-            //CurrentO = "Wait for future updates";
-            //addPast(UItext.text, CurrentO);
-            //UItext.text = CurrentO;
-            //Ctask++;
-            UpdateChapter("Wait for future updates");
+            CurrentO = "Wait for future updates";
+            addPast(UItext.text, CurrentO);
+            UItext.text = CurrentO;
+            task++;
         }
             
     }
@@ -40,13 +36,6 @@ public class ObjectiveController : MonoBehaviour {
 	void Update () {
 		
 	}
-    public void UpdateChapter(string chapname){
-        CurrentO = chapname;
-        addPast(UItext.text, CurrentO);
-        UItext.text = CurrentO;
-        Ctask++;
-        Sound.PlaySound();
-    }
     public void addPast(string past,string s)
     {
         PastText.text = PastText.text + "\n>" + past;
