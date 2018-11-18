6642158e499cd650b65c2dfce27c4f01f24d308b
KillerTrex1 <martmaster1@gmail.com>
2018-11-18 22:55:49 +0800

    SOME UI SHIT IDK WHY I CANT PUSH IN MASTER

---
+++ Movement+dialogue/Assets/VisualNovel/Scripts/DialogueManager.cs
@@ -0,0 +1,396 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    DialogueParser parser;
+
+    public string dialogue, characterName;
+    public int lineNum;
+    int pose;
+    string position;
+    string[] options;
+    public bool playerTalking;
+    public bool clickOnce = false;
+    List<Button> buttons = new List<Button>();
+
+    public Text dialogueBox;
+    public Text nameBox;
+    public GameObject choiceBox;
+    public AnimatedText animatedText;
+    public Animator transition;
+    public Animator charAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        dialogue = "";
+        characterName = "";
+        pose = 0;
+        position = "L";
+        playerTalking = true;
+        parser = GameObject.Find("DialogueParser").GetComponent<DialogueParser>();
+        animatedText = GameObject.Find("TextBox").GetComponent<AnimatedText>();
+        transition = GameObject.Find("AnimPanel").GetComponent<Animator>();
+        charAnimator = GameObject.Find("GamePanel").GetComponent<Animator>();
+        lineNum = -1;
+        //transition.SetTrigger("fadein");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!clickOnce)     //preloads background
+        {
+            lineNum++;
+            ShowDialogue();
+            print("CLICKONCE "+lineNum);
+            UpdateUI();
+            clickOnce = true;
+        }
+        if (!playerTalking)
+        {
+            ClearButtons();
+        }
+
+        if (!dialogueBox.GetComponent<AnimatedText>().done && !dialogueBox.GetComponent<AnimatedText>().cancel && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("return")))   //skips the dialogue
+        {
+            /*
+            if (parser.GetName(lineNum + 1) != characterName && !charAnimator.GetBool("fadeout" + characterName) && (charAnimator.GetBool("idle" + characterName) || charAnimator.GetBool("fadein" + characterName)))
+            {
+             charAnimator.SetBool("fadeout" + characterName, true);
+             charAnimator.SetBool("idle" + characterName, false);
+             charAnimator.SetBool("fadein" + characterName, false);
+            }*/
+            //SetAnimation();
+            dialogueBox.GetComponent<AnimatedText>().cancel = true;
+            print("CANCELLING TEXT " + lineNum);
+        }
+
+        else if((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("return")) && playerTalking == false && dialogueBox.GetComponent<AnimatedText>().done)   //if dialogue is already skipped OR already done, go to next line
+        {
+            
+            if (parser.GetName(lineNum) == characterName && !charAnimator.GetBool("fadeout" + characterName) && (charAnimator.GetBool("idle" + characterName) || charAnimator.GetBool("fadein" + characterName)))
+            {
+             charAnimator.SetBool("fadeout" + characterName, true);
+             charAnimator.SetBool("idle" + characterName, false);
+             charAnimator.SetBool("fadein" + characterName, false);
+            }
+            //SetAnimation();
+            dialogueBox.GetComponent<AnimatedText>().cancel = false;
+            lineNum++;
+            ShowDialogue();
+            print("ACTION "+lineNum);
+            UpdateUI();
+            //SetAnimation();
+        }
+
+    }
+
+    public void ShowDialogue()
+    {
+        ParseLine();
+    }
+
+    public void UpdateUI()
+    {
+        dialogueBox.text = dialogue;
+        dialogueBox.GetComponent<AnimatedText>().startAnim();
+        nameBox.text = characterName;
+    }
+
+    void ClearButtons()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            print("Clearing buttons");
+            Button b = buttons[i];
+            buttons.Remove(b);
+            Destroy(b.gameObject);
+        }
+    }
+
+    void ParseLine()
+    {
+        try
+        {
+            print(parser.GetContent(lineNum)+" PARSE LINE");
+        }
+        catch(System.Exception e)
+        {
+            print(e.StackTrace);
+        }
+        
+        if (parser.GetName(lineNum) != "Player")
+        {
+            if (parser.GetContent(lineNum) == "`loadchar")
+            {
+                playerTalking = false;
+                characterName = parser.GetName(lineNum);
+                pose = parser.GetPose(lineNum);
+                position = parser.GetPosition(lineNum);
+                DisplayImages();
+                //charAnimator.SetBool("idle" + characterName, false);
+                //charAnimator.SetBool("fadein" + characterName, false);
+                //charAnimator.SetBool("fadeout" + characterName, true);
+                lineNum++;
+                ShowDialogue();
+                UpdateUI();
+            }
+
+            else
+            {
+                playerTalking = false;
+                characterName = parser.GetName(lineNum);
+                dialogue = parser.GetContent(lineNum);
+                pose = parser.GetPose(lineNum);
+                position = parser.GetPosition(lineNum);
+                DisplayImages();
+            }
+        }
+        else
+        {
+            if (parser.GetContent(lineNum) == "`background")
+            {
+                playerTalking = false;
+                characterName = "";
+                dialogue = "";
+                pose = parser.GetPose(lineNum);
+                position = "";
+                ChangeBackground();
+            }
+            else if(parser.GetContent(lineNum) == "`jump")
+            {
+                lineNum = parser.GetPose(lineNum);
+                ShowDialogue();
+            }
+            else if(parser.GetContent(lineNum) == "`clear")
+            {
+                print("CLEARING "+ parser.GetPosition(lineNum));
+                ResetImages(parser.GetPosition(lineNum));
+                lineNum++;
+                ShowDialogue();
+                if (parser.GetContent(lineNum+1) != "`end")
+                {
+                    characterName = "";
+                    dialogue = "";
+                    UpdateUI();
+                }
+            }
+            else if(parser.GetContent(lineNum) == "`end")
+            {
+                endScene();
+            }
+            else
+            {
+                print("CREATING BUTTON");
+                playerTalking = true;
+                characterName = "";
+                dialogue = "";
+                pose = 0;
+                position = "";
+                options = parser.GetOptions(lineNum);
+                CreateButtons();
+            }        
+        }
+    }
+
+    void CreateButtons()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            GameObject button = (GameObject)Instantiate(choiceBox);
+            Button b = button.GetComponent<Button>();
+            ChoiceButton cb = button.GetComponent<ChoiceButton>();
+            cb.SetText(options[i].Split(':')[0]);
+            cb.option = options[i].Split(':')[1];
+            cb.box = this;
+            b.transform.SetParent(this.transform);
+            b.transform.localPosition = new Vector3(0, -25 + (i * 50));
+            b.transform.localScale = new Vector3(1, 1, 1);
+            buttons.Add(b);
+        }
+        if (playerTalking)
+        {
+            print("SHOULD PRINT");
+        }
+    }
+
+    void ResetImages(string charName)
+    {
+        print("RESETIMAGES" + charName);
+        GameObject character = GameObject.Find(charName);
+        SpriteRenderer currSprite = character.GetComponent<SpriteRenderer>();
+        currSprite.sprite = null;
+    }
+
+    void DisplayImages()
+    {
+        if (characterName != "")
+        {
+            SetAnimation();
+            print("DISPLAY " + characterName);
+            GameObject character = GameObject.Find(characterName);
+            SetSpritePositions(character);
+        }
+    }
+
+    void SetAnimation()
+    {
+        if((charAnimator.GetBool("fadein" + characterName) || charAnimator.GetBool("idle" + characterName)) && parser.GetName(lineNum + 1) != characterName)
+        //if ((charAnimator.GetCurrentAnimatorStateInfo(0).IsName("fadein" + characterName) || charAnimator.GetCurrentAnimatorStateInfo(0).IsName("idle" + characterName)))
+        {
+            charAnimator.SetBool("fadeout" + characterName, true);
+            charAnimator.SetBool("idle" + characterName, false);
+            charAnimator.SetBool("fadein" + characterName, false);
+            print("fadeout " + characterName);
+            //print("Animation playing: " + charAnimator.GetCurrentAnimatorStateInfo(0));
+            //print(parser.GetName(lineNum + 1) + "next name");
+        }
+
+        else if((charAnimator.GetBool("fadeout" + characterName) || charAnimator.GetBool("fadein" + characterName)) || (parser.GetName(lineNum + 1) == characterName && charAnimator.GetBool("idle" + characterName)))
+        //else if (charAnimator.GetCurrentAnimatorStateInfo(0).IsName("fadeout" + characterName))
+        {
+            charAnimator.SetBool("fadeout" + characterName, false);
+            //charAnimator.SetTrigger("idle" + characterName);
+            charAnimator.SetBool("idle" + characterName, true);
+            print("idle " + characterName);
+            //print("Animation playing: " + charAnimator.GetCurrentAnimatorStateInfo(0));
+        }
+
+        else if(!charAnimator.GetBool("fadein" + characterName))
+        //else if (charAnimator.GetCurrentAnimatorStateInfo(0).IsName("start" + characterName))
+        {
+            //charAnimator.SetTrigger("fadein" + characterName);
+            charAnimator.SetBool("fadein" + characterName, true);
+            print("fadein " + characterName);
+            //print("Animation playing: " + charAnimator.GetCurrentAnimatorStateInfo(0));
+            StartCoroutine(WaitOneSecondChar());
+            if(parser.GetContent(lineNum) == "`loadchar")
+            {
+                charAnimator.SetBool("fadeout" + characterName, true);
+                print("fadeout " + characterName);
+            }
+        }
+        //else
+        //{
+            //print("REEEE ANIMATION");
+            //charAnimator.SetTrigger("fadeinArRA");
+            //print("Animation playing: "+ charAnimator.GetCurrentAnimatorStateInfo(0));
+        //}
+    }
+
+    void SetSpritePositions(GameObject spriteObj)
+    {
+        SpriteRenderer currSprite = spriteObj.GetComponent<SpriteRenderer>();
+
+        if (position == "L")
+        {
+            spriteObj.transform.position = new Vector3(250, 280);
+        }
+        else if (position == "R")
+        {
+            spriteObj.transform.position = new Vector3(700, 280);
+        }
+        else if (position == "M")
+        {
+            spriteObj.transform.position = new Vector3(500, 280);
+        }
+        else if (position == "LF")  //F == flip
+        {
+            currSprite.flipX = true;
+            spriteObj.transform.position = new Vector3(250, 280);
+        }
+        else if (position == "RF")
+        {
+            currSprite.flipX = true;
+            spriteObj.transform.position = new Vector3(700, 280);
+        }
+        else if (position == "MF")
+        {
+            currSprite.flipX = true;
+            spriteObj.transform.position = new Vector3(500, 280);
+        }
+        spriteObj.transform.position = new Vector3(spriteObj.transform.position.x, spriteObj.transform.position.y, 0);
+
+        currSprite.sprite = spriteObj.GetComponent<Character>().characterPoses[pose];
+        //transition.SetTrigger("fadeinChar");
+    }
+
+    void ChangeBackground()
+    {
+        print("CHANGE BG");
+        GameObject bg = GameObject.Find("Background");
+        Texture bgTexture = (Texture)bg.GetComponent<BackgroundScript>().bg[pose];
+        Shader shader = Shader.Find("Unlit/Texture");
+        StartCoroutine(WaitAnimToFinish(bg, bgTexture));
+        bg.GetComponent<Renderer>().material.shader = shader; 
+        playerTalking = true;
+    }
+
+    IEnumerator WaitAnimToFinish(GameObject bg, Texture bgTexture)
+    {
+        if (clickOnce)
+        {
+            transition.SetBool("fadeout", true);
+
+            yield return new WaitForSeconds(1);
+
+            transition.SetBool("fadeout", false);
+            bg.GetComponent<Renderer>().material.mainTexture = bgTexture;
+            transition.SetBool("fadein", true);
+
+            yield return new WaitForSeconds(1);
+
+            transition.SetBool("fadein", false);
+        }
+        else
+        {
+            //print("linenum before " + lineNum);
+            bg.GetComponent<Renderer>().material.mainTexture = bgTexture;
+            yield return new WaitForSeconds(1);
+        }
+
+        //print("wait 1 sec");
+        lineNum++;
+        //print("linenum after " + lineNum);
+        ShowDialogue();
+        UpdateUI();
+        //playerTalking = false;
+    }
+
+    void endScene()
+    {
+        playerTalking = true;
+        StartCoroutine(WaitAnimToFinishEnd());
+    }
+
+    IEnumerator WaitAnimToFinishEnd()
+    {
+        transition.SetBool("fadeout", true);
+        yield return new WaitForSeconds(1);
+        transition.SetBool("fadeout", false);
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator WaitOneSecondChar()
+    {
+        //print("SUPPOSE TO SEE THIS ONCE");
+        if (!playerTalking)
+        {
+            //playerTalking = true;
+            yield return new WaitForSeconds(1);
+            //playerTalking = false;
+        }
+
+        else
+        {
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# Doki-Doki-Software-Club
+# READ ME REEEEEEEEEEEEEEEEEEEEEE
+## How to add dialogue
+1. Go to test/Assets and open dialogue.txt
+2. Write your dialogue in this format "ACTOR;DIALOGUE;INDEX#;L or R"
+    - ACTOR is the character Object you want load
+    - DIALOGUE is the string you want the actor to say
+    - INDEX# is the index number of the image in it's GameObject
+    - L or R means putting the image either on the left or right side of the screen
+    - Sample:
+      - Yukino;Hello world;0;L
+      - This sample uses actor "Yukino", uses her sprite in the 0th index, the text box will say Hello world and her sprite will be spawned at the left side.
+
+## How to add choices
+1. Go to test/Assets and open dialogue.txt
+2. Write the choices in this format "Player;CHOICE1:line,LINE#;CHOICE2:line,LINE#"
+    - CHOICE1 and CHOICE2 is what will be displayed in the buttons for the choices. **Choice doesn't have to be only 2, can have multiple choices.**
+    - LINE# is the line number the game will go to if that option is clicked. LINE# count begins with 0 so the first line in dialouge.txt is Line number 0. LINE# will be changed in the future since keeping track of all the line numbers is a problem.
+    - The lines "Player" and "line" shouldn't be changed as they tag what kind of command they do
+    - Sample:
+      - Player;Sader:line,1;Yukino:line,2
+      - This sample will display to the player 2 buttons labeled "Sader" and "Yukino". Clicking Sader will jump the game to line 1 in the .txt file while clicking Yukino will jump the game to line 2 in the .txt file. 
+      - Note that Yukino's button will appear at the top of Sader's button instead of the other way around. This will again be changed in the future for convinience.
+      
+## How to change the background
+1. Go to test/Assets and open dialogue.txt
+2. Write "Player;`background;INDEX#"
+    - Player and `background are tags to identify what kind of command is being done
+    - INDEX# is the index number of the background you want to load
+    - Sample:
+      - Player;`background;0
+      - This sample loads the picture in index 0 and sets it as the background.
+      
+## How to convert your images to sprites
+1. Click on your project tab
+2. Using the project tab find and click on the picture you want to convert to a sprite
+3. After finding your picture, check your inspector tab and click "Textture type"
+4. Select "Sprite (2D and UI)"
+5. Click "Apply" on the bottom of the tab
+
+## How to add a new character to the game
+1. Make sure all image that will be used are converted to sprites and the images are in Assets/Pictures
+2. Right-click on the "Hierarchy" tab
+3. Click "Create Empty"
+4. Name the GameObject the name of the character you want it to represent. Note that spelling and capitilization should be the same as the character name in dialogue.txt
+5. Click "Add Component" and click "Scripts" then click "Character"
+6. On your "Inspector" tab, you should see the character script. Click on "Character Poses" to extend it.
+7. On the textfield that says "size", enter the amount of sprites the char you want to add has.
+8. After entering a number above 1, it should add more textfields labeled "Element 0", "Element 1" and so on
+9. Click on the circle icon on the left of those text fields.
+10. Select the image you want to add to that index
+11. Repeat 9 and 10 until you add all your sprites
+12. Once done adding, click "Add Component" and click "Rendering" and click "Sprite Renderer"
+
+## How to add a new background to the game
+1. Make sure all image that will be used are converted to sprites and the images are in Assets/Pictures
+2. Click on "Background" on your "Hierarchy" tab
+3. On your "Inspector", look for the tab named "Background Script"
+4. Click on the "bg" tab to expand it
+5. Increase the number on the "size" tab by the number of backgrounds you will be adding
+5. Click on the circle on the right of the empty elements and select the image you want to add.
+6. Repeat 5 until all images are added
+7. Remember that their index number is the same number used to call them in dialogue.txt
