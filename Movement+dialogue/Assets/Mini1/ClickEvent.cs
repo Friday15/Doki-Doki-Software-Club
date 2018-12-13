087e6ac63de253fbc843274bb75caf372f9f1f11
KillerTrex1 <martmaster1@gmail.com>
2018-12-13 13:01:55 +0800

    DANGER^2

--- Movement+dialogue/Assets/Mini1/ClickEvent.cs
+++ Movement+dialogue/Assets/Mini1/ClickEvent.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,3 +18,24 @@ public class ClickEvent : MonoBehaviour {
         //img.gameObject.SetActive(false);
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickEvent : MonoBehaviour {
+    public Image img;
+	public Image baseColor;
+    private Color clr;
+    public void changeImage()
+    {
+        clr = new Color(197 / 255f, 40 / 255f, 61 / 255f, 255 / 255f);
+        if (img.GetComponent<Image>().color == clr)
+            img.GetComponent<Image>().color = baseColor.GetComponent<Image>().color;
+        else
+            img.GetComponent<Image>().color = clr;
+        //img.gameObject.SetActive(false);
+    }
+}
+>>>>>>> parent of cccb023... jalter nut
--- Movement+dialogue/Assets/Mini1/FinishClick.cs
+++ Movement+dialogue/Assets/Mini1/FinishClick.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -26,3 +27,33 @@ public class FinishClick : MonoBehaviour {
         //img.gameObject.SetActive(false);
     }
 }
+=======
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class FinishClick : MonoBehaviour {
+    
+    public Image img4;
+    public Image img5;
+    Color clr;
+    public void finishClick()
+    {
+        clr = new Color(197 / 255f, 40 / 255f, 61 / 255f, 255 / 255f);
+        if (img4.GetComponent<Image>().color ==clr && img5.GetComponent<Image>().color == clr)
+        {
+            String s = "REEEEEEEEEEE";
+            PlayerPrefs.SetString("str", s);
+            //PlayerPrefs.SetInt("int", 69);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene(6);
+        }else
+            SceneManager.LoadScene(10);
+        //img.gameObject.SetActive(false);
+    }
+}
+>>>>>>> parent of cccb023... jalter nut
