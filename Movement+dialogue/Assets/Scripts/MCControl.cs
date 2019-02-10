400539012140ebac0f7077437ef1c13bb5314ce5
ShouTucker <40710056+ShouTucker@users.noreply.github.com>
2019-02-10 22:38:53 +0800

    Added animations

--- Movement+dialogue/Assets/Scripts/MCControl.cs
+++ Movement+dialogue/Assets/Scripts/MCControl.cs
@@ -10,6 +10,7 @@ public class MCControl : MonoBehaviour
     public SpriteRenderer sr;
     public Sprite Other;
     public Trigger trigger;
+    public Animator animator;
     // Use this for initialization
 
 
@@ -18,7 +19,10 @@ public class MCControl : MonoBehaviour
         if (PlayerPrefs.HasKey("gender"))
         {
             if (PlayerPrefs.GetInt("gender") == 2)
+            {
                 sr.sprite = Other;
+                //animator.SetBool("isFemale", true);
+            }
         }
         if (PlayerPrefs.HasKey("x")&& PlayerPrefs.HasKey("y"))
             rg.position = new Vector2(PlayerPrefs.GetFloat("x"), PlayerPrefs.GetFloat("y"));
@@ -34,14 +38,41 @@ public class MCControl : MonoBehaviour
     {
         float hmove = Input.GetAxis("Horizontal");
         float vmove = Input.GetAxis("Vertical");
+
         if (trigger.isConversing == 0)
             rg.velocity = new Vector2(hmove * Mspeed, vmove * Mspeed);
         else
             rg.Sleep();
         PlayerPrefs.SetFloat("x", rg.position.x);
         PlayerPrefs.SetFloat("y", rg.position.y);
-        PlayerPrefs.SetInt("stage", stage);
-        
+        PlayerPrefs.SetInt("stage", stage);      
 
+    }
+
+    void FixedUpdate()
+    {
+        float speed = Input.GetAxis("Horizontal") * Mspeed;
+
+        if (sr != null)
+            animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        // if the A key was pressed this frame
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey("left"))
+        {
+            // if the variable isn't empty (we have a reference to our SpriteRenderer
+            if (sr != null)
+            {
+                // flip the sprite
+                sr.flipX = true;
+                //animator.SetBool("lookLeft", true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey("right"))
+        {
+            // flip the sprite
+            sr.flipX = false;
+            //animator.SetBool("lookLeft", false);
+        }
     }
 }
